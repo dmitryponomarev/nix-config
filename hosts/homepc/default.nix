6da362f6a407e032c0ce7ca52a99d5ee6867b47d
dmitryponomarev <dmitry.s.ponomarev@gmail.com>
2025-07-06 12:59:15 +0200

    test

--- hosts/homepc/default.nix
+++ hosts/homepc/default.nix
@@ -13,14 +13,14 @@
 
   networking.hostName = "homepc";
 
-  environment.variables = {
-    LIBVA_DRIVER_NAME = "nvidia";
-    NVD_BACKEND = "direct";
-    MOZ_DISABLE_RDD_SANDBOX = "1";
-    GBM_BACKEND = "nvidia-drm";
-    _GLX_VENDOR_LIBRARY_NAME = "nvidia";
-    WLR_DRM_NO_ATOMIC = "1";
-  };
+  # environment.variables = {
+  #   LIBVA_DRIVER_NAME = "nvidia";
+  #   NVD_BACKEND = "direct";
+  #   MOZ_DISABLE_RDD_SANDBOX = "1";
+  #   GBM_BACKEND = "nvidia-drm";
+  #   _GLX_VENDOR_LIBRARY_NAME = "nvidia";
+  #   WLR_DRM_NO_ATOMIC = "1";
+  # };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
@@ -34,7 +34,7 @@
   hardware.nvidia.modesetting.enable = true;
   hardware.nvidia.powerManagement.enable = false;
   hardware.nvidia.powerManagement.finegrained = false;
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_535;
 
   security.pam.services.swaylock = {};
 
