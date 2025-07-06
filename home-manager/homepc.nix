534839bdb6b5d37b9ccbd843a94dc4dbabb1c18f
dmitryponomarev <dmitry.s.ponomarev@gmail.com>
2025-07-06 16:15:29 +0200

    test

--- home-manager/homepc.nix
+++ home-manager/homepc.nix
@@ -16,7 +16,7 @@
     enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-  }
+  };
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
