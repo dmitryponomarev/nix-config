ae0e96c484ece55dbfc2946aed1664d29c0a01e7
dmitryponomarev <dmitry.s.ponomarev@gmail.com>
2025-09-23 09:47:37 +0200

    test

--- home-manager/dotfiles/swaylock/default.nix
+++ home-manager/dotfiles/swaylock/default.nix
@@ -1,13 +1,14 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+{
 
   programs.swaylock = {
     package = pkgs.swaylock-effects;
     enable = true;
     settings = {
-      image = ../../../wallpapers/wallpaper.png;
-      effect-greyscale = true;
-      effect-vignette = "0.3:0.8";
-      effect-blur = "3x3";
+      image = ../../../wallpapers/lockscreen.png;
+      # effect-greyscale = true;
+      # effect-vignette = "0.3:0.8";
+      # effect-blur = "3x3";
     };
   };
 }
